73d12f3b0086c191e3147462b4148c0f683d1921
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2024-12-10 11:13:10 -0500

    Bump version: 0.2.3.dev1 → 0.2.3

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/ml-stars',
-    version='0.2.3.dev1',
+    version='0.2.3',
     zip_safe=False,
 )
